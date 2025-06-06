081a9dea714d2c74bc815c146bf41dc918f1f6c3
w-odriguez <bernardo.guisande@wantel.com.br>
2025-06-06 13:34:42 -0300

    dev:started translating the go code to C++

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,9 +1,7 @@
 #include "MainWindow.h"
 #include "./ui_MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,64 @@
+#include <QApplication>
+#include <map>
+#include <string>
+
 #include "MainWindow.h"
 
-#include <QApplication>
+class ip_provider
+{
+private:
+	std::string url;
+	std::string method;
+	int ipv;
+	std::map<std::string, std::string> opt;
+	ip_provider_response res;
+
+public:
+	std::string ip;
+};
+
+// IPIFY ----------------------------------------------------------------------
+class IPIFY: ip_provider
+{
+private:
+	std::string url = "https://api64.ipify.org";
+	std::string method = "GET";
+	int ipv = 6;
+	std::map<std::string, std::string> opt;
+	ip_provider_response res;
+
+public:
+	virtual std::string parse(std::string response)
+	{
+		this->ip = response;
+		return response;
+	}
+};
+
+// IFCFG ----------------------------------------------------------------------
+class IFCFG: ip_provider
+{
+private:
+	std::string url = "https://ifcfg.me/";
+	std::string method = "GET";
+	int ipv = 4;
+	std::map<std::string, std::string> opt;
+	ip_provider_response res;
+
+public:
+	virtual std::string parse(std::string response)
+	{
+		this->ip = response;
+		return response;
+	}
+}
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+	std::string PUBLIC_IP_PROVIDERS[2] = { "ifcfg", "ipify" };
+
+	QApplication a(argc, argv);
+	MainWindow w;
+	w.show();
+	return a.exec();
 }
